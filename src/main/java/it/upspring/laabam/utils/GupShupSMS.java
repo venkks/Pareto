71ccba7864b7847ce669d9ef9e7ae01f199d4836
venkks <venkks@gmail.com>
2018-10-11 11:24:01 +0530

    Create GupShupSMS.java

---
+++ src/main/java/it/upspring/laabam/utils/GupShupSMS.java
@@ -0,0 +1,50 @@
+package it.upspring.laabam.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Date;
+
+public class GupShupSMS {
+
+    public static void sendSMS(String mobileNo, String message){
+        try {
+            Date mydate = new
+                    Date(System.currentTimeMillis());
+            String data = "";
+            data += "method=sendMessage";
+            data += "&userid=2000153229"; // your loginId
+            data += "&password=" +
+                    URLEncoder.encode("8pMp70tQe", "UTF-8"); // your password
+            data += "&msg=" + URLEncoder.encode(message, "UTF-8");
+                    data += "&send_to=" +
+                            URLEncoder.encode(mobileNo, "UTF-8"); // a valid 10 digit phone no.
+            data += "&v=1.1" ;
+            data += "&msg_type=TEXT"; // Can by "FLASH" or"UNICODE_TEXT" or “BINARY”
+            data += "&auth_scheme=PLAIN";                    URL url = new
+                    URL("http://enterprise.smsgupshup.com/GatewayAPI/rest?" + data);
+            HttpURLConnection conn =
+                    (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            conn.connect();
+            BufferedReader rd = new BufferedReader(new
+                    InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuffer buffer = new StringBuffer();
+            while ((line = rd.readLine()) != null){
+                buffer.append(line).append("\n");
+            }
+            System.out.println(buffer.toString());
+            rd.close();
+            conn.disconnect();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        } }
+}
